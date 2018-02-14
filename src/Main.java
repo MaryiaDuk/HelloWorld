ec996f744b9143c0c9a83b0f566c3f5073c64008
Maria Duk <mashaduk1996@gmail.com>
2018-02-15 01:25:06 +0300

    Hello World from master again!

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World from master branch!");
         System.out.println("Hello World from dev branch!");
         System.out.println("Hello World from dev branch again!");
+        System.out.println("Hello World from master branch again!");
     }
 }
